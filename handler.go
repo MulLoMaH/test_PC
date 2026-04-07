fe6b7916389833d480869ff69cf35f4b8f4399a8
B_O_R_M_A_N <mullomahsg@gmail.com>
2026-04-07 13:48:12 +0300

    for halloWorld

--- handler.go
+++ handler.go
@@ -1,12 +1,27 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"sync"
+	"time"
+)
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	s := []byte("haLLO")
 
-	if _, err := w.Write(s); err != nil {
-		return
-	}
+	wg := sync.WaitGroup{}
 
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 3; i++ {
+			time.Sleep(1 * time.Second)
+			if err := json.NewEncoder(w).Encode(map[string]string{"Hallo": "World"}); err != nil {
+				log.Println("error", err)
+			}
+		}
+	}()
+
+	wg.Wait()
 }
